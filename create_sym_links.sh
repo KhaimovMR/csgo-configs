e79e2c349d08cea9b39ec7b1241432b289d93a4a
Michael Khaimov <mhaimov@firstgaming.com>
2017-09-08 17:08:06 +0300

    Create sym links for linux.

--- create_sym_links.sh
+++ create_sym_links.sh
@@ -1,35 +1,79 @@
 cs_found=0
 cs_files_found=0
 insufficient_permissions=1
-default_cs_paths="~/.steam/steam/steamapps/common/Counter-Strike\ Global\ Offensive"
+default_cs_paths=("$HOME/.steam/steam/steamapps/common/Counter-Strike Global Offensive")
+repo_path=`dirname $(readlink -f $(type -p $0))`
 
-if [[ "$1" == "" ]]
+function pause() {
+    read -p "Press ENTER to continue..."
+}
+
+function folders_loop() {
+	if [ -e "$1/csgo/cfg" ]
+    then
+        echo "CS:GO found on $1 path"
+
+		if [ ! -e "$1/csgo/cfg/my_overrides.cfg" ]
+        then
+			echo "// Write your configuration overrides below" > "$1/csgo/cfg/my_overrides.cfg"
+        fi
+
+		for cfg_file_path in $(ls -1 $2/cfg/*.cfg)
+        do 
+            filename=$(basename $cfg_file_path)
+			
+			if [ -e "$1/csgo/cfg" ]
+            then
+				cs_found=1
+
+				if [ -L "$1/csgo/cfg/$filename" ]
+                then
+                    echo An old symbolic link for "$1/csgo/cfg/$filename" is found. Trying to remove it... &&
+                    rm "$1/csgo/cfg/$filename"
+                fi
+
+                if [ -e "$1/csgo/cfg/$filename" ]
+                then
+                    cs_files_found=0
+                    echo Found a file "$filename" instead of link in directory: "$1/csgo/cfg".
+                    echo Please remove it and restart this script.
+                    echo
+                    pause
+                    exit 1
+                fi
+				
+				if [ ! -e "$1/csgo/cfg/$filename" ]
+                then
+					ln -s "$2/cfg/$filename" "$1/csgo/cfg/$filename" && insufficient_permissions=0
+					echo
+                fi
+            fi
+        done
+    fi
+}
+
+if [[ "$1" != "" ]]
 then 
 	default_cs_paths="$1"
 	echo Using specified path.
 	echo
-	echo
-	echo
 else
 	echo Using a default paths...
 	echo
-	echo
-	echo
 fi		
 
-for $p in $default_cs_paths
+#for p in $default_cs_paths
+for (( i=0; i<${#default_cs_paths[@]}; i++ ));
 do
-	folders_loop $p %~dp0cfg
+	folders_loop "${default_cs_paths[$i]}" "$repo_path"
 done
 
-if $insufficient_permissions == 1
+if [[ $insufficient_permissions == 1 ]]
 then
-	echo
 	echo
 	echo This script should be runned under Administrator privileges.
 	echo
-	echo
-	sleep 15
+    pause
     exit 0
 fi
 
@@ -43,72 +87,21 @@ then
 	if [[ "$1" != "" ]]
     then
 		echo
-		echo
 		echo CS:GO was not found in specified path.
 		echo
-		sleep 15
+        pause
 	else
-		echo
 		echo
 		echo CS:GO was not found in your system.
 		echo
-		sleep 15
+        pause
     fi
 else
-	echo
 	echo
 	echo CS GO was succesfully found and links to configs are created.	
 	echo
 	echo Note:
-	echo  You can override the default autoexec configuration in a file "CSGO-DIR/csgo/cfg/my_overrides.cfg"
+	echo   You can override the default autoexec configuration in a file "CSGO-DIR/csgo/cfg/my_overrides.cfg"
 	echo
-	pause
+    pause
 fi
-
-function folders_loop() {
-	if [ -e "$1/csgo/cfg" ]
-    then
-		if [ ! -e "$1/csgo/cfg/my_overrides.cfg" ]
-        then
-			echo "// Write your configuration overrides below" > "$1/csgo/cfg/my_overrides.cfg"
-        fi
-
-		for /r %2 %%f in (*.cfg)
-        do 
-			set filename=%%~nxf
-			
-			if exist "$1/csgo/cfg"
-            then
-				cs_found=1
-				if exist "$1/csgo/cfg/%%~nxf"
-                then
-					dir "%~1\csgo\cfg\%%~nxf" | find "<SYMLINK>" >NUL 2>&1 && (
-						echo An old symbolic link for "%~1\csgo\cfg\%%~nxf" is found. Trying to remove it... 
-						del "%~1\csgo\cfg\%%~nxf"
-					)
-					
-					if exist "%~1\csgo\cfg\%%~nxf"
-                    then
-						set _cs_files_found=1
-						echo Found a file "%%~nxf" instead of link in directory: "%~1\csgo\cfg".
-						echo Please remove it and restart this script.
-						echo
-						echo
-						echo
-						sleep 15
-						echo
-						echo
-						echo
-						exit 1
-                    fi
-                fi
-				
-				if not exist "%~1\csgo\cfg\%%~nxf"
-                then
-					mklink "%~1\csgo\cfg\%%~nxf" "%%~f" && set _insufficient_permissions=0
-					echo.
-                fi
-            fi
-        done
-    fi
-}
