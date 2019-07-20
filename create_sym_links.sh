dc2f37ac2656f8a9e8072b1ac23b7ccf37b972f2
Michael Khaimov <khaimovmr@gmail.com>
2019-07-20 16:56:22 +0300

    finalize the sym links script for linux

--- create_sym_links.sh
+++ create_sym_links.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 cs_found=0
 cs_files_found=0
 insufficient_permissions=1
@@ -9,24 +10,27 @@ function pause() {
 }
 
 function folders_loop() {
-	if [ -e "$1/csgo/cfg" ]
-    then
+    echo "$1"
+    echo "$2"
+    #ls -la "$1/csgo/cfg"
+
+    if [[ -e "$1/csgo/cfg" ]]; then
         echo "CS:GO found on $1 path"
 
-		if [ ! -e "$1/csgo/cfg/my_overrides.cfg" ]
+        if [ ! -e "$1/csgo/cfg/my_overrides.cfg" ]
         then
-			echo "// Write your configuration overrides below" > "$1/csgo/cfg/my_overrides.cfg"
+            echo "// Write your configuration overrides below" > "$1/csgo/cfg/my_overrides.cfg"
         fi
 
-		for cfg_file_path in $(ls -1 $2/cfg/*.cfg)
-        do 
+        for cfg_file_path in $(ls -1 $2/cfg/*.cfg)
+        do
             filename=$(basename $cfg_file_path)
-			
-			if [ -e "$1/csgo/cfg" ]
-            then
-				cs_found=1
 
-				if [ -L "$1/csgo/cfg/$filename" ]
+            if [ -e "$1/csgo/cfg" ]
+            then
+                cs_found=1
+
+                if [ -L "$1/csgo/cfg/$filename" ]
                 then
                     echo An old symbolic link for "$1/csgo/cfg/$filename" is found. Trying to remove it... &&
                     rm "$1/csgo/cfg/$filename"
@@ -41,11 +45,11 @@ function folders_loop() {
                     pause
                     exit 1
                 fi
-				
-				if [ ! -e "$1/csgo/cfg/$filename" ]
+
+                if [ ! -e "$1/csgo/cfg/$filename" ]
                 then
-					ln -s "$2/cfg/$filename" "$1/csgo/cfg/$filename" && insufficient_permissions=0
-					echo
+                    ln -s "$2/cfg/$filename" "$1/csgo/cfg/$filename" && insufficient_permissions=0
+                    echo
                 fi
             fi
         done
@@ -53,55 +57,55 @@ function folders_loop() {
 }
 
 if [[ "$1" != "" ]]
-then 
-	default_cs_paths="$1"
-	echo Using specified path.
-	echo
+then
+    default_cs_paths="$1"
+    echo Using specified path.
+    echo
 else
-	echo Using a default paths...
-	echo
-fi		
+    echo Using a default paths...
+    echo
+fi
 
 #for p in $default_cs_paths
 for (( i=0; i<${#default_cs_paths[@]}; i++ ));
 do
-	folders_loop "${default_cs_paths[$i]}" "$repo_path"
+    folders_loop "${default_cs_paths[$i]}" "$repo_path"
 done
 
 if [[ $insufficient_permissions == 1 ]]
 then
-	echo
-	echo This script should be runned under Administrator privileges.
-	echo
+    echo
+    echo This script should be runned under Administrator privileges.
+    echo
     pause
     exit 0
 fi
 
 if [[ $cs_files_found == 1 ]]
 then
-	exit 0
+    exit 0
 fi
 
 if [[ $cs_found == 0 ]]
 then
-	if [[ "$1" != "" ]]
+    if [[ "$1" != "" ]]
     then
-		echo
-		echo CS:GO was not found in specified path.
-		echo
+        echo
+        echo CS:GO was not found in specified path.
+        echo
         pause
-	else
-		echo
-		echo CS:GO was not found in your system.
-		echo
+    else
+        echo
+        echo CS:GO was not found in your system.
+        echo
         pause
     fi
 else
-	echo
-	echo CS GO was succesfully found and links to configs are created.	
-	echo
-	echo Note:
-	echo   You can override the default autoexec configuration in a file "CSGO-DIR/csgo/cfg/my_overrides.cfg"
-	echo
+    echo
+    echo CS GO was succesfully found and links to configs are created.
+    echo
+    echo Note:
+    echo   You can override the default autoexec configuration in a file "CSGO-DIR/csgo/cfg/my_overrides.cfg"
+    echo
     pause
 fi
